c1365c36ad3fa74e9007b6245815fda6d89588a6
axlgoze <rbaxel.f@gmail.com>
2024-05-01 09:33:08 -0600

    1) MODIFY: code desc. added

--- my-element.js
+++ my-element.js
@@ -1,16 +1,20 @@
+// especificacion de modulos con js 'import'
+// la clase LitElement simplifica la creación de componentes reutilizables al proporcionar funcionalidades como manejo de propiedades, ciclo de vida y renderizado eficiente.
+// html template is used to render
 import {LitElement, html} from 'lit';
-
+// Aquí se define el comportamiento y la estructura del componente MyElement. 
 export class MyElement extends LitElement{
     static properties = {
         version : {},
     };
-
 
     constructor(){
         super();
         this.version = 'World';
     }
 
+    // render is mandatory
+    // define el contenido HTML que será renderizado por el componente. 
     render(){
         return html`
         <p>Hello</p>
@@ -19,4 +23,5 @@ export class MyElement extends LitElement{
     }
 }
 
+// define() registers your custom element with the browser as a new element type
 customElements.define('my-element', MyElement);
