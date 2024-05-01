9b1425f4f6b5ff628e0391c97c685df0a2312769
axlgoze <rbaxel.f@gmail.com>
2024-05-01 09:58:53 -0600

    3) DECLARATIVE EVENT LISTENERS

--- my-element.js
+++ my-element.js
@@ -1,29 +1,40 @@
-// especificacion de modulos con js 'import'
-// la clase LitElement simplifica la creación de componentes reutilizables al proporcionar funcionalidades como manejo de propiedades, ciclo de vida y renderizado eficiente.
-// html template is used to render
+// DECLARATIVE EVENT LISTENERS
+
 import {LitElement, html} from 'lit';
-// Aquí se define el comportamiento y la estructura del componente MyElement. 
+
 export class MyElement extends LitElement{
     
-    // properties of the component
+    
     static properties = {
-        message : {},
+        name : {},
     };
-    // reactive propertie
+    
     constructor(){
         super();
-        this.message = 'hello again';
+        this.name = 'Axel Reyes';
     }
 
-    // render is mandatory
-    // define el contenido HTML que será renderizado por el componente. 
+    
     render(){
         return html`
-        <p>Hey</p>
-        <p>${this.message}.</p>
+        <p>Hey ${this.name}</p>
+        
+        // declarative event listener.
+        <input @input=${this.changeName} placeholder="Enter your name">
+
+        // event handlers To add interactivity to your components
+        <button  @click=${this.handleClick} >Click me!</button>
+        
         `;
     }
+
+    // event handler method.
+    changeName(event) {
+        const input = event.target;
+        this.name = input.value;
+      }
+
 }
 
-// define() registers your custom element with the browser as a new element type
+
 customElements.define('my-element', MyElement);
