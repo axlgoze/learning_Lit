1e205b13c91e146223185be0c50bdeb4096492fb
axlgoze <rbaxel.f@gmail.com>
2024-05-01 11:22:29 -0600

    5) TEMPLATE LOGIC

--- my-element.js
+++ my-element.js
@@ -1,29 +1,52 @@
-// MORE EXPRESSIONS
+// TEMPLATE LOGIC
 
 import {LitElement, html} from 'lit';
 
-export class MoreExpressions extends LitElement {
+export class ToDoList extends LitElement {
   static properties = {
-    checked: {},
+    _listItems: {state: true},
   };
 
   constructor() {
     super();
-    this.checked = false;
+    // asignando un array de objetos
+    this._listItems = [
+    // dos objetos con propiedades
+      {text: 'Start Lit tutorial', completed: true},
+      {text: 'Make to-do list', completed: false},
+    ];
   }
 
   render() {
     return html`
-      <div>
-         <!-- The ?attributeName syntax tells Lit you want to set or remove a boolean attribute based on the value of the expression. -->
-         <input ?disabled=${!this.checked} type="text" value="Hello there.">
-      </div>
-      <label><input type="checkbox" @change=${this.setChecked}> Enable editing</label>
+      <h2>To Do</h2>
+      <ul>
+         <!-- TODO: Render list items. -->
+        // aplica funcion map()
+        ${this._listItems.map((item) =>
+            html`<li>${item.text}</li>`
+        )}
+      </ul>
+        
+      <input id="newitem" aria-label="New item">
+      <button @click=${this.addToDo}>Add</button>
     `;
   }
+  
+  //click handler
+  get input() {
+    return this.renderRoot?.querySelector('#newitem') ?? null;
+    }
 
-  setChecked(event) {
-    this.checked = event.target.checked;
+  // TODO: Add click handler.
+  addToDo() {
+    // operador de propagacion para crear un nuevo array. Esto es una forma de hacer una copia del array original antes de modificarlo.
+    this._listItems = [...this._listItems,
+        {text: this.input.value, completed: false}];
+      this.input.value = '';
   }
+
+  //   Mutating objects and arrays.
+
 }
-customElements.define('more-expressions', MoreExpressions);
+customElements.define('todo-list', ToDoList);
