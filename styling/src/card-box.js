539f42727bc3292c57e7345d1c9762b4562683e2
axlgoze <rbaxel.f@gmail.com>
2024-06-03 16:55:56 -0600

    40)EIT:shared styles

--- styling/src/card-box.js
+++ styling/src/card-box.js
@@ -1,8 +1,9 @@
 import { LitElement, html, css } from 'lit';
+import { sharedStyles } from './shared-styles.js';
 
 export class CardBox extends LitElement {
-    static styles = [
-        css`
+    static get styles() {
+        return [sharedStyles, css`
             :host {
                 display: block;
             }
@@ -10,8 +11,10 @@ export class CardBox extends LitElement {
             div {
                 border: solid 1px red;
             }
+            
         `
-    ];
+        ];
+    }
 
     render() {
         return html`
